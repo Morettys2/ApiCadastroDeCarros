81c22812c5deeb1a77db0fe6d68025c076de18de
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-03-12 16:08:21 -0300

    Fazendo o service do projeto

--- src/main/java/dev/moretty/ApiCadastroDeCarros/repository/repositoryCarros.java
+++ src/main/java/dev/moretty/ApiCadastroDeCarros/repository/repositoryCarros.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface  repositoryCarros extends JpaRepository<modelCarros, Long> {
+public interface repositoryCarros extends JpaRepository<modelCarros, Long> {
 }
--- src/main/java/dev/moretty/ApiCadastroDeCarros/service/serviceCarros.java
+++ src/main/java/dev/moretty/ApiCadastroDeCarros/service/serviceCarros.java
@@ -1,4 +1,12 @@
 package dev.moretty.ApiCadastroDeCarros.service;
 
+import dev.moretty.ApiCadastroDeCarros.repository.repositoryCarros;
+import org.springframework.stereotype.Service;
+
+@Service
 public class serviceCarros {
+
+    private repositoryCarros repo;
+
+
 }
