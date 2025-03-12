a523d41b8cdaff092a781d13aa4fad0a796cc786
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-03-12 16:32:00 -0300

    Rota inciial da api/carros criada

--- src/main/java/dev/moretty/ApiCadastroDeCarros/controller/controllerCarros.java
+++ src/main/java/dev/moretty/ApiCadastroDeCarros/controller/controllerCarros.java
@@ -1,4 +1,10 @@
 package dev.moretty.ApiCadastroDeCarros.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/carros")
 public class controllerCarros {
+
+
 }
