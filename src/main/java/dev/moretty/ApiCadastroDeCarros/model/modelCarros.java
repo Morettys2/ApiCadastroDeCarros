04a8583f1be22130b9784e410d001152b85636f2
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-03-12 15:59:26 -0300

    Model do projeto finalizado

--- src/main/java/dev/moretty/ApiCadastroDeCarros/model/modelCarros.java
+++ src/main/java/dev/moretty/ApiCadastroDeCarros/model/modelCarros.java
@@ -1,50 +1,27 @@
 package dev.moretty.ApiCadastroDeCarros.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "carros")
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class modelCarros {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
     private String marca;
     private String modelo;
     private String ano;
     private String cor;
+    private String imgUrl;
 
-    public String getMarca() {
-        return marca;
-    }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
 
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getAno() {
-        return ano;
-    }
-
-    public void setAno(String ano) {
-        this.ano = ano;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
 }
