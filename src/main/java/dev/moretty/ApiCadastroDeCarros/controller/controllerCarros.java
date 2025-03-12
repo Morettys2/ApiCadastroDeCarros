51b152b0af4f2cce36e5c42afd28251f827836fe
Joao Pedro Moretti <m0rettyy20@gmail.com>
2025-03-12 16:41:20 -0300

    Rota do Post com /add como rota criado

--- src/main/java/dev/moretty/ApiCadastroDeCarros/controller/controllerCarros.java
+++ src/main/java/dev/moretty/ApiCadastroDeCarros/controller/controllerCarros.java
@@ -1,10 +1,27 @@
 package dev.moretty.ApiCadastroDeCarros.controller;
+
+import dev.moretty.ApiCadastroDeCarros.model.modelCarros;
+import dev.moretty.ApiCadastroDeCarros.service.serviceCarros;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/carros")
 public class controllerCarros {
+
+    @Autowired
+    private serviceCarros service;
+
+    @PostMapping("/add")
+    public ResponseEntity<modelCarros> addCarros(@RequestBody modelCarros carro) {
+     modelCarros newCarro = service.addCar(carro);
+     return new ResponseEntity<>(newCarro,HttpStatus.CREATED);
+    }
 
 
 }
